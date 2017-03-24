70b6a3ba87f5bdd5a400d20bbf7b3a02c6eae20d
Matthias Koenig <konigmatt@googlemail.com>
2017-03-24 15:33:30 +0100

    tox file & contribution added, setup.py updated, Fix #16, Fix#14

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 """
 Setup module for libsbgn.
 
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-
 source distribution generation via
 python setup.py sdist
 """
 
 # Always prefer setuptools over distutils
+from __future__ import absolute_import, print_function
 from setuptools import setup, find_packages
 from codecs import open  # To use a consistent encoding
 from os import path
@@ -18,7 +15,6 @@ import re
 here = path.abspath(path.dirname(__file__))
 
 # get the version
-
 VERSIONFILE = "libsbgnpy/_version.py"
 verstrline = open(VERSIONFILE, "rt").read()
 VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
@@ -43,30 +39,14 @@ setup(
 
     description='libsbgn python bindings',
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/matthiaskoenig/libsbgn-python',
-
-    # Author details
-    author='Matthias Koenig',
+    author='Matthias König',
     author_email='konigmatt@googlemail.com',
-
-    # Choose your license
-    license='LGPL-v3',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    license='LGPLv3',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Visualization',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
 
         # Specify the Python versions you support here. In particular, ensure
@@ -76,38 +56,18 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
-    keywords='SBGN libsbgn',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    # exclude=['contrib', 'docs', 'tests*']
+    keywords='SBGN, libsbgn',
     packages=find_packages(),
+    install_requires=[
+        'enum34',
+        'lxml',
+        'six'
+    ],
 
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['enum34'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    # extras_require={
-    #     'dev': ['check-manifest'],
-    #    'test': ['coverage'],
-    # },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # package_data={
-    #    'sample': ['package_data.dat'],
-    # },
-    
     # include the package data (SBGN, XSD)
     include_package_data=True,
 
