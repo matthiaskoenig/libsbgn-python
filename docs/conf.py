a20d2fd89101b14998219b8cdc54e26e367657ce
Matthias Koenig <konigmatt@googlemail.com>
2017-03-25 18:42:20 +0100

    sphinx must be able to import the lirary

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
-
+sys.path.insert(0, os.path.abspath('..'))
 
 # In order to build documentation that requires libraries to import
 class Mock(object):
